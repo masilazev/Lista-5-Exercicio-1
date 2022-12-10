7f3e58387fb09b1e649705b7242e4616c841b28a
Matheus Azevedo <matheus2002azevedo@icloud.com>
2022-12-10 13:17:07 -0300

    first commit

---
+++ Exercicio1.java
@@ -0,0 +1,14 @@
+public class Exercicio1 {
+    
+    public static void main(String[] args){
+    int n = 9;
+    int i;
+    int j;
+        for(i = 1; i <= n; i++){
+            for(j=0; j<i; j++){
+             System.out.print(i+" ");
+            }
+            System.out.println(i+ " ");
+        }
+    }
+}
